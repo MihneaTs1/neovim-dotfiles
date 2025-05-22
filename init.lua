87f4131808a983055b62c433e0e71365680d3132
MihneaTs1 <stoicamihnea2009@gmail.com>
2025-05-22 15:07:03 +0300

    update

--- init.lua
+++ init.lua
@@ -1,4 +1,17 @@
 require("config.plg")
 require("plg").setup({
-
+	{
+		plugin = "https://github.com/folke/tokyonight.nvim"
+	},
+	{
+		plugin = "https://github.com/nvim-treesitter/nvim-treesitter",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				sync_install = true,
+				auto_install = true,
+				highlight = { enable = true }
+			})
+			-- error("lot")
+		end
+	}
 })
--- lua/config/plg.lua
+++ lua/config/plg.lua
@@ -3,7 +3,6 @@ local fn = vim.fn
 local install_path = fn.stdpath('data') .. '/site/pack/plg/start/plg.nvim'
 
 if fn.empty(fn.glob(install_path)) > 0 then
-  print("Installing plg.nvim…")
   fn.system({
     'git', 'clone', '--depth', '1',
     'https://github.com/MihneaTs1/plg.nvim',
