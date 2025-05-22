b8ab98a2af7e68ad4fc6cc17314c47181d110b58
MihneaTs1 <stoicamihnea2009@gmail.com>
2025-05-22 14:36:37 +0300

    update

--- init.lua
+++ init.lua
@@ -1,24 +1,4 @@
-vim.loader.enable()
-
--- Custom Plugin Manager
-
 require("config.plg")
+require("plg").setup({
 
--- Editor Settings
-
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.softtabstop = 4
-
-vim.opt.wrap = false
-
-vim.opt.number = true
-vim.opt.relativenumber = true
-
-vim.opt.cursorline = true
-vim.opt.cursorcolumn = false
-
-vim.opt.clipboard = "unnamedplus"
-
--- Custom Persistent Colorscheme
-require("keepcolor")
+})
